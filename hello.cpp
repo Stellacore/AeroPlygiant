2808604e887057a2209c45aec33229ba3ed3f94e
Stellacore <dave@stellacore.com>
2023-11-29 12:06:00 -0700

    CMakeLists.txt,hello.cpp - build example using Engabra

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+
+
+#include <Engabra>
+
+#include <iostream>
+
+int
+main
+	()
+{
+	std::cout << "Hello Refraction\n";
+	std::cout << "Using Engabra Project: " << engabra::projectVersion() << '\n';
+	std::cout << "Using Engabra Source: " << engabra::sourceIdentity() << '\n';
+}
+
