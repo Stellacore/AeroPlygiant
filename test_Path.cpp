d50af89eff6ddf776243e2c863eb1935f8ed3a27
Stellacore <dave@stellacore.com>
2023-12-07 13:38:56 -0700

    test_Path.cpp - working ray trace (and test) for thick slab

--- test_Path.cpp
+++ test_Path.cpp
@@ -18,6 +18,34 @@
 
 namespace tst
 {
+
+	/*! CMake/CTest compatible exit code based on condition (! msg.empty())
+	 *
+	 * If message string is empty, return success
+	 */
+	inline
+	int
+	result
+		( std::string const & msg
+		)
+	{
+		int istat{ 1 };
+		if (msg.empty())
+		{
+			istat = 0;
+		}
+		else
+		{
+			std::cerr << msg << '\n';
+		}
+		return istat;
+	}
+
+} // [tst]
+
+
+namespace tst
+{
 	using namespace engabra::g3;
 
 	/*! \brief Simple example of a spherical shape with constant index.
@@ -143,6 +171,26 @@ namespace tst
 
 	}; // Slab
 
+	//! Evaluate index of refraction on line through media
+	void
+	showMedia
+		( env::IndexVolume const & media
+		)
+	{
+		constexpr double stepDist{ 1./ 4. };
+		for (double dist{0.} ; dist < 10. ; dist += stepDist)
+		{
+			Vector const rCurr{ dist, 5., 5. };
+			double const nuCurr{ media.nuValue(rCurr) };
+			Vector const gCurr{ media.nuGradient(rCurr, stepDist) };
+			std::cout
+				<< "  dist: " << io::fixed(dist)
+				<< "  nuCurr: " << io::fixed(nuCurr)
+				<< "  gCurr: " << io::fixed(gCurr)
+				<< std::endl;
+		}
+	}
+
 } // [tst]
 
 
@@ -173,38 +221,76 @@ main
 		, 1.5  // nu inside
 		, 1.0  // nu outside
 		);
+//	tst:showMedia(media);
 
-	// basic construction of path
+	// path specification
 	Vector const tBeg{ 1., 1., 1. };
 	Vector const rBeg{ 0., 0., 0.  };
 	Vector const stopNear{ 10., 10., 10. };
-	constexpr double saveStepDist{ 1./1. }; // save this often
-	save::Path path(tBeg, rBeg, stopNear, saveStepDist);
+
+	// configuration
+	constexpr double propStepDist{ 1./4096 }; // integration step size
+	constexpr double saveStepDist{ 1./8. }; // save this often
 
 	// create tracer
-//	constexpr double propStepDist{ .001 }; // integration step size
-constexpr double propStepDist{ 1./2. }; // integration step size
 	ray::Propagator const prop{ &media, propStepDist };
 
 	// interact with data consumer
-	prop.traceNodes(tBeg, rBeg, &path);
+	save::Path path(tBeg, rBeg, stopNear, saveStepDist);
+	prop.traceNodes(path.theBegTan, path.theBegLoc, &path);
 
-	std::cout << "path.size: " << path.theNodes.size() << std::endl;
-	for (ray::Node const & node : path.theNodes)
+	// show path info
+//	constexpr bool showIt{ true };
+	constexpr bool showIt{ false };
+	if (showIt)
 	{
-//		std::cout << node.infoBrief() << std::endl;
+		std::cout << "path.size: " << path.theNodes.size() << std::endl;
+		for (ray::Node const & node : path.theNodes)
+		{
+			std::cout << node.infoBrief() << std::endl;
+		}
 	}
 
-oss << "Failure of tangent Path test\n";
-	if (oss.str().empty())
+	// check that path contains data
+	if (! (2u < path.size()))  // size of 2 required for test below
 	{
-		istat = 0;
+		oss << "Failure of path size test\n";
+		oss << "path.size: " << path.size() << '\n';
 	}
 	else
 	{
-		std::cerr << oss.str() << '\n';
+		// check that path exiting the slab is parallel to one entering
+		ray::Node const & begNode = path.theNodes.front();
+		ray::Node const & endNode = path.theNodes.back();
+		Vector const & begDir = begNode.thePrevTan;
+		Vector const & endDir = endNode.theNextTan;
+		if (! nearlyEquals(begDir, endDir))
+		{
+			oss << "Failure of begin/end direction test\n";
+			oss << "begDir: " << begDir << '\n';
+			oss << "endDir: " << endDir << '\n';
+		}
+
+		// check that internal ray direction is distinct
+		// (assuming block is near center of path)
+		std::size_t const midNdx{ path.size() / 2u };
+		ray::Node const & midNode = path.theNodes[midNdx];
+		Vector const inDir{ .5*(midNode.thePrevTan + midNode.theNextTan) };
+		Vector const exDir{ .5*(begNode.thePrevTan + endNode.theNextTan) };
+		Spinor const relSpin{ inDir * exDir };
+		constexpr double significantDeflection{ .1 };
+		double const bivMag{ magnitude(relSpin.theBiv) };
+		if (! (significantDeflection < bivMag))
+		{
+			oss << "Failure of internal significant deflection test\n";
+			oss << "inDir: " << inDir << std::endl;
+			oss << "exDir: " << exDir << std::endl;
+			oss << "relSpin: " << relSpin << std::endl;
+			oss << " bivTol: " << significantDeflection << std::endl;
+			oss << " bivMag: " << bivMag << std::endl;
+		}
 	}
 
-	return istat;
+	return tst::result(oss.str());
 }
 
