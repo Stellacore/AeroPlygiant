460d7b934bc6c2d4a554d5444b743904ef435f6f
Stellacore <dave@stellacore.com>
2023-12-07 21:31:19 -0700

    demoThickPlate.cpp nominally working

--- demoThickPlate.cpp
+++ demoThickPlate.cpp
@@ -17,14 +17,38 @@
 
 #include <Engabra>
 
+#include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 
 
 namespace
 {
 	using namespace engabra::g3;
+
+	//! Generate a bundle of ray starting elements
+	std::vector<ray::Start>
+	rayStarts
+		( Vector const & station
+		)
+	{
+		std::vector<ray::Start> starts;
+		static double const xVal{ .5 };
+		static std::vector<double> const yVals{ .00, .25, .50, .75, 1. };
+		static std::vector<double> const zVals{ .00, .25, .50, .75, 1. };
+		starts.reserve(zVals.size() * yVals.size());
+		for (double const & yVal : yVals)
+		{
+			for (double const & zVal : zVals)
+			{
+				starts.emplace_back
+					(ray::Start::from(Vector{ xVal, yVal, zVal }, station));
+			}
+		}
+		return starts;
+	}
 
 } // [anon]
 
@@ -53,21 +77,28 @@ main
 	ray::Propagator const prop{ &media, propStepDist };
 
 	// path specification
-	Vector const station{ 0., 0., 0. };
-	Vector const lookAlong{ 1., 1., 1. };
-	ray::Start const start{ ray::Start::from(lookAlong, station) };
+	Vector const station{  0.,  0.,  0. };
 	Vector const stopNear{ 10., 10., 10. };
 
-	// interact with data consumer
-	ray::Path path(start, stopNear, saveStepDist);
-	prop.traceNodes(path.theStart, &path);
+	// starting rays to trace
+	std::vector<ray::Start> const starts{ rayStarts(station) };
 
-	// show path info
-	for (ray::Node const & node : path.theNodes)
+	// trace and report each ray
+std::ofstream ofs("/tmp/dk.tmp/foo");
+	for (ray::Start const & start : starts)
 	{
-		std::cout << node.infoBrief() << std::endl;
+		// interact with data consumer
+		ray::Path path(start, stopNear, saveStepDist);
+		prop.traceNodes(path.theStart, &path);
+
+		// show path info
+		for (ray::Node const & node : path.theNodes)
+		{
+			ofs << node.infoBrief() << std::endl;
+		}
+		ofs << "\n\n\n";
+		std::cout << "path.size: " << path.theNodes.size() << std::endl;
 	}
-	std::cout << "path.size: " << path.theNodes.size() << std::endl;
 
 }
 
