5620d3c602c34e925bdcbd4550087c2ce573f381
Stellacore <dave@stellacore.com>
2023-12-14 12:30:12 -0700

    demoHotRoad.cpp - corrected index of refraction error (use refractivity not IoR)

--- demoHotRoad.cpp
+++ demoHotRoad.cpp
@@ -47,7 +47,20 @@ namespace road
 		//! Cylindrical tube of (linearly) varying air IoR
 		aply::geom::Cylinder const theTube;
 		//! Index of refraction gap from axis to outside radial edge
-		aply::geom::Interval const theGapNu;
+		aply::geom::Interval const theNuInterval;
+
+		//! Create (fake) index of refraction gradient in radial direction
+		inline
+		static
+		aply::geom::Interval
+		nuInterval
+			()
+		{
+			double const nuGround{ aply::env::sEarth.theNuGround };
+			double const refractivity{ nuGround - 1. }; // refractivity (IoR-1.)
+			double const nuAxis{ 1. + .5*refractivity };
+			return aply::geom::Interval(nuAxis, nuGround);
+		}
 
 		//! Construct this shape and alignment
 		inline
@@ -57,10 +70,7 @@ namespace road
 			)
 			: IndexVolume{}
 			, theTube{ tube }
-			, theGapNu
-				{ .5 * aply::env::sEarth.theNuGround  // IoR along axis
-				,      aply::env::sEarth.theNuGround  // IoR along outer edge
-				}
+			, theNuInterval{ nuInterval() }
 		{ }
 
 		//! Index of refraction associated with radial gradient along cylinder
@@ -70,14 +80,14 @@ namespace road
 			( engabra::g3::Vector const & rLoc
 			) const
 		{
-			double nu{ theGapNu.max() }; // default to STP air
+			double nu{ theNuInterval.max() }; // default to STP air
 			double const lenFrac{ theTube.fractionAlongAxis(rLoc) };
 			if ((! (lenFrac < 0.)) && (lenFrac < 1.))
 			{
 				double const radFrac{ theTube.fractionFromAxis(rLoc) };
 				if (radFrac < 1.)
 				{
-					nu = theGapNu.valueAtFrac(radFrac);
+					nu = theNuInterval.valueAtFrac(radFrac);
 				}
 			}
 			return nu;
@@ -109,12 +119,17 @@ main
 
 	// scene configuration
 	static Vector const axisDir{ e1 };
-	static Vector const dirToRight{ -e2 };
 	constexpr double hotRadius{ 10. }; // [m]
+	constexpr double length{ 1000. }; // [m]
+	static Vector const staDir{ -e2 }; // direction to station from origin
+	static Vector const obsDir{ axisDir }; // observation direction
+	static Vector const approxEndLoc{ length * obsDir };
 
 	// tracing configuration
-	constexpr double propStepDist{ .001 }; // Propagation step size[m]
-	constexpr double saveStepDist{ .001 }; // Data save step size[m]
+//	constexpr double propStepDist{ .001 }; // Propagation step size[m]
+//	constexpr double saveStepDist{ .001 }; // Data save step size[m]
+constexpr double propStepDist{  10. }; // Propagation step size[m]
+constexpr double saveStepDist{ 100. }; // Data save step size[m]
 
 	using namespace aply;
 
@@ -123,23 +138,32 @@ main
 		{ geom::Cylinder
 			( zero<Vector>()  // axis starting at origin
 			, axisDir         // axis along the positive 'x' direction
-			, 1000.           // length [m]
+			, length          // length [m]
 			, hotRadius       // radius [m]
 			)
 		};
 
+// TODO
+// NOTE: the initial index of refraction is starting at "air" value
+//       (somewhere inside Propagator maybe?)
+//       should take starting NU value from environment.
+
 	// setup ray start parallel to cylinder 'off to the side' of the 'road'
 	ray::Start const start
-		{ ray::Start::from(axisDir, .5*hotRadius*dirToRight) };
+		{ ray::Start::from(obsDir, .5*hotRadius*staDir + 1.e-6*e1) };
 
 	// construct propagator
 	ray::Propagator const prop{ &media, propStepDist };
 
 	// trace path
-	ray::Path path(start, saveStepDist);
+	ray::Path path(start, saveStepDist, approxEndLoc);
 	prop.tracePath(&path);
 
 	// report results
+	for (ray::Node const & node : path.theNodes)
+	{
+		std::cout << node.infoBrief() << '\n';
+	}
 	std::cout << ray::PathView(&path).infoCurvature() << std::endl;
 
 }
